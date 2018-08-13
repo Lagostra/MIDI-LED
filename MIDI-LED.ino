25a4270f43b42771695464b1e6f45c00c6cd8244
Eivind Lie Andreassen <ela4991@gmail.com>
2018-08-13 19:59:00 +0200

    Add light fade when pedal is down

--- MIDI-LED.ino
+++ MIDI-LED.ino
@@ -10,13 +10,18 @@
 #define OFFSET 0
 #define SUM 1
 #define FLOOR_SUM 2
+#define DIM_TIME 1500
 
 
 int mode = FLOOR_SUM;
 
 
 CRGB leds[NUM_LEDS];
+CRGB colors[NUM_LEDS];
 int midiNotes[128];
+long releaseTimes[128];
+bool pedalDown = false;
+bool hasChanged = false;
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -36,6 +41,11 @@ void loop()
 }
 
 void updateLeds() {
+  if(!hasChanged && !pedalDown) {
+    return;  
+  }
+  hasChanged = false;
+  
   int offset = 0;
   int notesPerLed = 1;
 
@@ -55,14 +65,28 @@ void updateLeds() {
   for(int i = 0; i < NUM_LEDS; i++) {
     int j = 0;
     int ledSum = 0;
+    long newestReleaseTime = 0;
     while(j < notesPerLed && (j + i * notesPerLed + 21) < 109) {
+      newestReleaseTime = max(newestReleaseTime, releaseTimes[j + i * notesPerLed + 21 + offset]);
       ledSum += midiNotes[j + i * notesPerLed + 21 + offset];
       j++;
     }
     if(ledSum == 0) {
-      leds[i] = NULL;  
+      if(pedalDown) {
+        long timeSinceRelease = millis() - newestReleaseTime;
+        if(timeSinceRelease >= DIM_TIME) {
+          leds[i] = NULL;  
+        } else {
+          leds[i] = colors[i];
+          int dimFactor = int((float(timeSinceRelease) / DIM_TIME) * 256);
+          leds[i].fadeLightBy(dimFactor);
+        }
+      } else {
+        leds[i] = NULL;
+      }
     } else {
-      leds[i] = getColor(ledSum);  
+      leds[i] = getColor(ledSum);
+      colors[i] = leds[i];
     }
   }
   
@@ -70,15 +94,36 @@ void updateLeds() {
 }
 
 void checkMIDI(){
-  while(!MIDI.read()) {} // Wait for next MIDI event
+  // while(!MIDI.read()) {} // Wait for next MIDI event
   do {
+    hasChanged = true;
+    int n;
     switch(MIDI.getType())      // Get the type of the message we caught
     {
         case midi::NoteOn:
             midiNotes[MIDI.getData1()] = MIDI.getData2();
             break;
         case midi::NoteOff:
-            midiNotes[MIDI.getData1()] = 0;
+            n = MIDI.getData1();
+            midiNotes[n] = 0;
+            if(pedalDown) {
+              releaseTimes[n] = millis(); 
+            }
+            break;
+        case midi::ControlChange:
+            switch(MIDI.getData1()) {
+              case 64:
+                if(MIDI.getData2() >= 64) {
+                  pedalDown = true;
+                } else {
+                  // Reset all release times to avoid waking lights next time pressing pedal
+                  for(int i = 0; i < 128; i++) {
+                    releaseTimes[i] = 0;  
+                  }
+                  pedalDown = false;
+                }
+                break;
+            }
             break;
         default:
             break;
